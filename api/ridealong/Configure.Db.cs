da70b0526fe8b15274a4e15b59a77e5d840ad4a1
binit <dhurbecha@gmail.com>
2024-06-27 15:32:34 +0545

    project setup

---
+++ api/ridealong/Configure.Db.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+[assembly: HostingStartup(typeof(ridealong.ConfigureDb))]
+
+namespace ridealong;
+
+public class ConfigureDb : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices((context, services) => {
+            services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
+                context.Configuration.GetConnectionString("DefaultConnection")
+                ?? "Server=localhost;User Id=postgres;Password=password;Database=postgres",
+                PostgreSqlDialect.Provider));
+        })
+        .ConfigureAppHost(appHost => {
+            // Enable built-in Database Admin UI at /admin-ui/database
+            // appHost.Plugins.Add(new AdminDatabaseFeature());
+        });
+}
